5f849bca960da42b65e9f599bfc745f46dbd9a0c
Chris Straight <christopher.straight@fdmgroup.com>
2024-03-25 10:37:07 +1100

    inline elements are no longer on new lines

--- src/renderer.py
+++ src/renderer.py
@@ -10,6 +10,11 @@ class Renderer:
         self.canvas = tk.Canvas(parent, width=width, height=height, bg=StyleDefaults.backgroundColour)
         self.x_location = 0
         self.y_location = 0
+        self.x_next = 0
+        self.y_next = 0
+        self.line_number = 0
+        self.line = []
+        self.lines = []
 
     def render(self, root):
         self.canvas.delete('all')
@@ -41,6 +46,7 @@ class Renderer:
         elif node.tag == 'body':
             pass
         elif node.tag == 'div':
+            self.new_line(top_spacing + bottom_spacing)
             new_styling['font'] = StyleDefaults.userInterfaceFont
             new_styling['foreground'] = StyleDefaults.primaryColour
         elif node.tag == 'span':
@@ -49,13 +55,14 @@ class Renderer:
             new_styling['foreground'] = StyleDefaults.link_colour
             new_styling['underline'] = True
         elif node.tag == 'table':
-            pass
+            self.new_line(top_spacing + bottom_spacing)
         elif node.tag == 'p':
-            pass
+            self.new_line(top_spacing + bottom_spacing)
         elif node.tag == 'pre':
             new_styling['font'] = StyleDefaults.monospaceFont
         elif (node.tag == 'h1' or node.tag == 'h2' or node.tag == 'h3' or node.tag == 'h4' or node.tag == 'h5'
               or node.tag == 'h6'):
+            self.new_line(top_spacing + bottom_spacing)
             new_styling['font'] = StyleDefaults.headingFont
             if node.tag[1] == str(1):
                 new_styling['size'] = StyleDefaults.h1FontSize
@@ -76,10 +83,11 @@ class Renderer:
                 new_styling['size'] = StyleDefaults.h6FontSize
                 new_styling['foreground'] = StyleDefaults.h6Colour
         elif node.tag == 'hr':
-            pass
+            self.new_line(top_spacing + bottom_spacing)
         elif node.tag == 'br':
             pass
         elif node.tag == 'ul':
+            self.new_line(top_spacing + bottom_spacing)
             if 'indent' not in new_styling:
                 new_styling['indent'] = ''
             if 'list_level' not in new_styling:
@@ -89,10 +97,12 @@ class Renderer:
             new_styling['indent'] += '\t'
             new_styling['list_level'] += 1
         elif node.tag == 'ol':
+            self.new_line(top_spacing + bottom_spacing)
             new_styling['indent'] = '\t'
             new_styling['list_level'] = 1
             new_styling[f'index{new_styling['list_level']}'] = 0
         elif node.tag == 'li':
+            self.new_line(top_spacing + bottom_spacing)
             for style in styling:
                 if style == 'foreground':
                     foreground = styling[style]
@@ -107,13 +117,13 @@ class Renderer:
                 anchor=tk.NW,
                 fill=foreground,
                 text=new_styling['indent'] + new_styling['mark'],
-                font=font,
-                width=600
+                font=font
             )
             bounds = self.canvas.bbox(text_element)
             self.x_location = bounds[2]
             elements += (text_element,)
         elif node.tag == 'blockquote':
+            self.new_line(top_spacing + bottom_spacing)
             new_styling['font'] = StyleDefaults.serifFont
             new_styling['background'] = StyleDefaults.pale_green
             new_styling['foreground'] = StyleDefaults.accentColour2
@@ -155,19 +165,33 @@ class Renderer:
                 fill=foreground,
                 text=text,
                 font=font,
-                width=600
             )
 
             bounds = self.canvas.bbox(text_element)
 
             bg_element = self.canvas.create_rectangle((bounds[0], bounds[1]), (bounds[2], bounds[3]), fill=background)
             self.canvas.tag_lower(bg_element)
-            self.y_location = bounds[3] + bottom_spacing
-            self.x_location = 0
+            # self.y_location = bounds[3] + bottom_spacing
+            self.x_location = bounds[2] + end_spacing
             elements += ((text_element, bg_element),)
+
+            self.line.append((text_element, bg_element))
 
         for child in node.children:
             elements += self.render_node(child, new_styling)
 
         # print(elements)
         return elements
+
+    def new_line(self, line_spacing):
+        self.lines.append(self.line)
+        # self.line = []
+        self.line_number += 1
+        self.x_location = 0
+        bottom_of_line = 0
+        for element in self.line:
+            text, bg = element
+            bounds = self.canvas.bbox(text)
+            if bounds[3] > bottom_of_line:
+                bottom_of_line = bounds[3]
+        self.y_location = bottom_of_line + line_spacing
